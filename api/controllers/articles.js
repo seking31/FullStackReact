431d55346af9e304811705dc7121b44c6c4cc22b
Sara King <sara.king@wwt.com>
2021-08-08 21:39:33 -0400

    changed folder structure, css edits

--- api/controllers/articles.js
+++ api/controllers/articles.js
@@ -8,13 +8,12 @@ const apiKey = process.env.API_KEY;
 
 articlesRouter.post('/', function (req, res) {
     let searchQuery = req.body.searchQuery
-    console.log(searchQuery, 3124134)
     axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=${apiKey}`)
         .then(data => res.json(data.data.articles))
         .catch(err => console.log(err));
 });
 
-articlesRouter.get('/search', function (req, res) {
+articlesRouter.get('/', function (req, res) {
     axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
         .then(data => res.json(data.data.articles))
         .catch(err => console.log(err));
